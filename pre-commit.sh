e3c4f5b061745eb8e912b7c69218cfa935dc6e37
Yuki Murakami <ymurakami@keio.jp>
2020-05-08 20:21:04 +0900

    Modify pre-commit.sh

--- pre-commit.sh
+++ pre-commit.sh
@@ -2,9 +2,13 @@
 
 echo 'Run Sample script'
 
-# コミットされるファイルのうち、.phpで終わるもの
-for FILE in `git diff --name-only 52a3d603e6df49996ece5fe305e48c025c334570`; do
-  echo 'hoge'
-  echo $FILE
-  # git add $FILE
+# コミットされるファイルのうち、.mdで終わるもの
+ITEMS_PATH=items
+for FILE in `git diff --name-only 52a3d603e6df49996ece5fe305e48c025c334570 --relative=$ITEMS_PATH | grep item.md` ; do
+  DIR_NAME=`dirname $FILE`
+  echo $DIR_NAME
+  echo $ITEMS_PATH/$DIR_NAME
+  # git add $ITEMS_PATH/$DIR_NAME
 done
+
+# git add -A
